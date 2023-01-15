cfaee6e65d0fd6acde05cdd6fc331d7a9d49c53d
Carlos Reynaga <carandreyn@gmail.com>
2023-01-15 14:19:44 -0800

    changed title

--- src/Components/Layout/Header.jsx
+++ src/Components/Layout/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
   return (
     <div className='py-3 pl-2' style={{borderBottom: "1px solid #777"}}>
           <img src={logo} alt="" style={{ height: "35px", verticalAlign: "top", marginRight: "15px"}} />
-          <span className='h2 pt-2 text-white-50'>React Course - Contactopedia</span>
+          <span className='h2 pt-2 text-white-50'>React - Contacts Page</span>
     </div>
   )
 }
